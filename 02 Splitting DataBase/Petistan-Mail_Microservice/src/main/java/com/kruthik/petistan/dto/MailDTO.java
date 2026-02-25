5f97b753ad125e0702e1dce74204e0ff09d263b4
kruthik_sp <kruthiksp62@gmail.com>
2026-02-25 15:51:17 +0530

    Delete 02 Splitting DataBase directory

--- 02 Splitting DataBase/Petistan-Mail_Microservice/src/main/java/com/kruthik/petistan/dto/MailDTO.java
+++
@@ -1,7 +0,0 @@
-package com.kruthik.petistan.dto;
-
-import com.kruthik.petistan.enums.MailType;
-
-public record MailDTO(String to, String firstName, String lastName, MailType category) {
-
-}
--- 02 Splitting DataBase/Petistan-Owner_Microservice/src/main/java/com/kruthik/petistan/service/MailService.java
+++
@@ -1,7 +0,0 @@
-package com.kruthik.petistan.service;
-
-import com.kruthik.petistan.dto.MailDTO;
-
-public interface MailService {
-	String sendEmail(MailDTO mailDTO);
-}
